c2b1b5e67ce18dcb6bb16600f06385b2b9f66b9c
bigrobsf <rjconner@mac.com>
2016-11-14 20:42:56 -0800

    created new transaction form

--- routes/index.js
+++ routes/index.js
@@ -12,17 +12,19 @@ const { camelizeKeys, decamelizeKeys } = require('humps');
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
-
+// =============================================================================
 // show home page
 router.get('/', function(req, res) {
   res.render('index');
 });
 
+// =============================================================================
 // show about page
 router.get('/site/about', function(req, res) {
   res.render('about');
 });
 
+// =============================================================================
 // show contact page
 router.get('/site/contact', function(req, res) {
   res.render('contact');
