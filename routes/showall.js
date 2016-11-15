7834e9d23fe87726d3d9be50203b2d1c36ed2722
David Shin <davidshin444@gmail.com>
2016-11-15 15:17:15 -0800

    Made showall pages w/ initial charts & app-dave.js testing router

---
+++ routes/showall.js
@@ -0,0 +1,22 @@
+/* jshint esversion: 6 */
+/* jshint devel: true */
+/* jshint node: true */
+/* jshint browser: true */
+'use strict';
+
+const boom  = require('boom');
+const express = require('express');
+const bcrypt = require('bcrypt-as-promised');
+var knex = require('../db/knex');
+// var yahooFinance = require('yahoo-finance');
+const { camelizeKeys, decamelizeKeys } = require('humps');
+// eslint-disable-next-line new-cap
+const router = express.Router();
+
+// =============================================================================
+// show showall page
+router.get('/', function(req, res) {
+  res.render('showall');
+});
+
+module.exports = router;
