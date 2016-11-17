cf23c4570015cd0892c81b47dc34c97265e8a55a
David Shin <davidshin444@gmail.com>
2016-11-17 11:14:58 -0800

    Clean - Done with basic 'open' positions

--- routes/positions.js
+++ routes/positions.js
@@ -17,31 +17,6 @@ const router = express.Router();
 // =============================================================================
 // Build open positions prices
 /**
-* Data for testing
-*/
-let openArr = [
-  {ticker: "AAPL",
-  buyPrice: 10,
-  buyDate: 'Sep 23 2014',
-  numShares: 200},
-
-  {ticker: "GS",
-  buyPrice: 14,
-  buyDate: 'Sep 23 2014',
-  numShares: 350},
-
-  {ticker: "JPM",
-  buyPrice: 10,
-  buyDate: 'May 3 2014',
-  numShares: 400},
-
-  {ticker: "AMZN",
-  buyPrice: 10,
-  buyDate: 'Jun 16 2014',
-  numShares: 100}
-]
-
-/**
 FIELD codes
 n: name
 l1: lastTradePriceOnly
@@ -60,15 +35,6 @@ console.log('OPEN POSITIONS!!');
 
 // =============================================================================
 // Define Position class
-
-// * This is for test data
-// var Position = function(ticker, buyPrice, buyDate, numShares) {
-//   this.ticker = ticker,
-//   this.buyPrice = buyPrice,
-//   this.buyDate = new Date(buyDate),
-//   this.numShares = numShares
-// }
-
 var Position = function(userName, ticker, sharePrice, tradeDate, numShares) {
   this.userName = userName,
   this.ticker = ticker,
@@ -76,9 +42,6 @@ var Position = function(userName, ticker, sharePrice, tradeDate, numShares) {
   this.tradeDate = new Date(tradeDate),
   this.numShares = numShares
 }
-
-
-
 
 // =============================================================================
 // show all open positions for current user
@@ -91,32 +54,14 @@ router.get('/open', function(req, res) {
   .where('closed_flag', false)
   .then((rows) => {
     if (req.cookies['/token']) {
-    //   const faves = camelizeKeys(rows);
     //   console.log("Robs Rows", rows);
-
-      //<- BEGIN inserting code from old showall.js here
-
       // Build symbol (to get quotes) and open position arrays
-      // * each for example data
-    //   _.each(openArr, function(stock) {
-    //       let newPos = new Position(stock.ticker, stock.buyPrice, stock.buyDate, stock.numShares)
-    //       openPositions.push(newPos);
-    //       symbols.push(newPos.ticker);
-    //   })
-      console.log("START");
-    //   console.log("typeof rows", typeof rows);
-    //   console.log("Array.isArray(rows)", Array.isArray(rows));
       _.each(rows, function(stock) {
-        //   console.log("MIDDLE stockObj", stock);
-        //   console.log("MIDDLE stock.id", stock.id);
-        //   console.log("MIDDLE stock.user_name", stock.user_name);
           let newPos = new Position(stock.user_name, stock.ticker, stock.share_price, stock.trade_date, stock.num_shares)
-        //   console.log("stuck here?")
+
           openPositions.push(newPos);
           symbols.push(newPos.ticker);
-        //   console.log("openPositions in _.each", openPositions);
       })
-      console.log("END");
 
       // Get realtime quotes
       yahooFinance.snapshot({
@@ -130,28 +75,21 @@ router.get('/open', function(req, res) {
           currPos.change = snapshot.change;
           currPos.changeInPercent = (snapshot.changeInPercent * 100).toFixed(2);
           currPos.glDollar = (Number(snapshot.lastTradePriceOnly * currPos.numShares) - Number(currPos.sharePrice * currPos.numShares)).toFixed(2);
-        //   currPos.glChangeInPercent = snapshot.changeInPercent * 100;
           currPos.glInPercent = (currPos.glDollar/(Number(snapshot.lastTradePriceOnly * currPos.numShares)) * 100).toFixed(2);
           currPos.dividendYield = snapshot.dividendYield;
           currPos.peRatio = snapshot.peRatio;
-          console.log("openPositions: ", openPositions)
         });
+        console.log("openPositions: ", openPositions)
         res.render('showall', {openPositions: openPositions})
       });
-      //<- END inserting code from old showall.js here
     } else {
       res.status(401);
       res.set('Content-Type', 'text/plain');
       res.send('Unauthorized');
     }
-
   }).catch((err) => {
-
     res.status(401).send(err);
   });
 });
-
-
-
 
 module.exports = router;
