447507a6aec9930e3110c09d8cb171216bb6e8b3
David Shin <davidshin444@gmail.com>
2016-12-06 13:00:41 -0800

    Working on cleaning up colored stats

--- public/js/closedall.js
+++ public/js/closedall.js
@@ -5,6 +5,7 @@
   */
   function colorPrices(conditionClass, targetClass) {
     for(var i = 0, j = targetClass.length; i < j; ++i) {
+      console.log(conditionClass[i].innerText);
       if (Number(conditionClass[i].innerText) > 0) {
         targetClass[i].style.color = "green";
       } else if (Number(conditionClass[i].innerText) < 0) {
@@ -16,37 +17,43 @@
   }
 
   // Change the color of the $change and $change columns
-  // var maxDrawDown = document.getElementsByClassName("maxDrawDown");
-  // colorPrices(maxDrawDown, maxDrawDown);
-  //
-  // var maxPortfolioGain = document.getElementsByClassName("maxPortfolioGain");
-  // colorPrices(maxPortfolioGain, maxPortfolioGain);
-  //
-  // var totalGl = document.getElementsByClassName("totalGl");
-  // colorPrices(totalGl, totalGl);
-  //
-  // var totalGlPercent = document.getElementsByClassName("totalGlPercent");
-  // colorPrices(totalGlPercent, totalGlPercent);
+  var maxDrawDown = document.getElementsByClassName("maxDrawDown");
+  colorPrices(maxDrawDown, maxDrawDown);
+
+  var maxPortfolioGain = document.getElementsByClassName("maxPortfolioGain");
+  colorPrices(maxPortfolioGain, maxPortfolioGain);
+
+  var totalGl = document.getElementsByClassName("totalGl");
+  colorPrices(totalGl, totalGl);
+
+  var totalGlPercent = document.getElementsByClassName("totalGlPercent");
+  colorPrices(totalGlPercent, totalGlPercent);
 
   // var change = document.getElementsByClassName("class_glAmount");
   // colorPrices(change, change);
   // var change = document.getElementsByClassName("class_glAmount");
   // colorPrices(change, change);
-
-
-
-
 
   var change = document.getElementsByClassName("class_glAmount");
-  colorPrices(change, change);
-
   var changeInPercent = document.getElementsByClassName("class_glInPercent");
-  colorPrices(changeInPercent, changeInPercent);
-
   var glDollar = document.getElementsByClassName("class_FolioAmount");
-  colorPrices(glInPercent, glDollar);
-
   var glInPercent = document.getElementsByClassName("class_FolioAmountPercent");
+  colorPrices(change, change);
+  colorPrices(changeInPercent, changeInPercent);
+  colorPrices(glInPercent, glDollar);
   colorPrices(glInPercent, glInPercent);
+
+
+  // var change = document.getElementsByClassName("class_glAmount");
+  // colorPrices(change, change);
+  //
+  // var changeInPercent = document.getElementsByClassName("class_glInPercent");
+  // colorPrices(changeInPercent, changeInPercent);
+  //
+  // var glDollar = document.getElementsByClassName("class_FolioAmount");
+  // colorPrices(glInPercent, glDollar);
+  //
+  // var glInPercent = document.getElementsByClassName("class_FolioAmountPercent");
+  // colorPrices(glInPercent, glInPercent);
 
 })();
